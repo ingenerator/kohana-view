bb420825b375f4fda197178abf7e0a4cf67a6d7c
Andrew Coulton <andrewnfcoulton@gmail.com>
2011-07-12 23:51:00 +0100

    Added test for runtime var method call efficiency

--- tests/test_view_model.php
+++ tests/test_view_model.php
@@ -13,12 +13,38 @@ class View_Model_Test extends Kohana_Unittest_TestCase
 	$expected = file_get_contents(Kohana::find_file('tests', 'output/test/escape', 'txt'));
 	$this->assertSame($expected, $view->render());
     }
+
+    public function test_methods_on_render()
+    {
+        $view = new View_Test_RuntimeMethods();
+        $view->render();
+        $this->assertTrue($view->called);
+        $this->assertFalse($view->not_called);
+    }
 }
 
 class View_Test_Escape extends View_Model
 {
-	public function var_foo()
-	{
-		return '<h2>foobar</h2>';
-	}
+    public function var_foo()
+    {
+        return '<h2>foobar</h2>';
+    }
+}
+
+class View_Test_RuntimeMethods extends View_Model
+{
+    public $called = false;
+    public $not_called = false;
+
+    public function var_called()
+    {
+        $called = true;
+        return true;
+    }
+
+    public function var_not_called()
+    {
+        $not_called = true;
+        return true;
+    }
 }

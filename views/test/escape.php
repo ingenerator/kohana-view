684c7fc0795e54ef99dcf02a6e5bc73110b2d0f2
Andrew Coulton <andrewnfcoulton@gmail.com>
2011-08-07 22:22:55 +0100

    Fix whitespace issues in escape test and expected output

--- views/test/escape.php
+++ views/test/escape.php
@@ -1,14 +1,19 @@
 <!--Raw Output-->
 <h1>Test</h1>
-<!--Unescaped class var -->
+<!--Unescaped class var-->
 <?=^$foo?>
+
 <!--Escaped class var-->
 <?=$foo?>
+
 <!--Unescaped class var with ;-->
 <?=^$foo;?>
+
 <!--Escaped class var with ;-->
 <?=$foo;?>
+
 <!--Loop with local var -->
 <?php foreach (array('1','2','3') as $tmp_array_var):?>
-    <?=$foo . $tmp_array_var;?>
+<?=$foo . $tmp_array_var;?>
+
 <?php endforeach; ?>

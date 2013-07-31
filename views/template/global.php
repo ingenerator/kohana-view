5a2338244bb4384097541666434f8ca19ebc1fc4
Andrew Coulton <andrewnfcoulton@gmail.com>
2013-07-31 21:32:55 +0100

    Add docblock to basic global template
    Best practice is to use the docblock comment to explicitly
    define all the variables that are available and provide a link
    to the expected view class.

--- views/template/global.php
+++ views/template/global.php
@@ -1,4 +1,12 @@
-<?php defined('SYSPATH') or die('No direct script access.');?>
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Basic global template view, rendered by the View_Template_Global class.
+ *
+ * @see View_Template_Global
+ * @var string $title Page title
+ * @var string $body  Page body HTML
+ */
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
     <head>

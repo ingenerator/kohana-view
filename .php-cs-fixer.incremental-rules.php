016ac11d491035c2a4695c0015059887bf324143
acoulton <andrew@ingenerator.com>
2025-10-02 10:50:47 +0000

    cs: Fix statement indentation

--- .php-cs-fixer.incremental-rules.php
+++ .php-cs-fixer.incremental-rules.php
@@ -1,4 +1,9 @@
 <?php
 return array (
   'encoding' => true,
+  'indentation_type' => true,
+  'statement_indentation' => 
+  array (
+    'stick_comment_to_next_continuous_control_statement' => true,
+  ),
 );

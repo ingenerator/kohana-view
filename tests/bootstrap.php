7ba38c087dde801ed346ba2755723c0fe2e8aeda
acoulton <andrew@ingenerator.com>
2025-10-02 12:21:28 +0100

    style: Use native function invocation

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // Bootstrap for running unit tests
-\define('TEST_ROOT_PATH', \realpath(__DIR__).'/');
+define('TEST_ROOT_PATH', realpath(__DIR__).'/');
 require_once __DIR__.'/../koharness_bootstrap.php';
 require_once KOHARNESS_SRC.'helper_classes/Session/Fake.php';
 

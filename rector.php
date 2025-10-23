d44a78e5b3f56cb5fbe7a6cbe7738da77e795411
acoulton <andrew@ingenerator.com>
2025-10-23 17:51:35 +0100

    feat: Add desired overall Rector config

--- rector.php
+++ rector.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
+use Rector\TypeDeclaration\Rector\Function_\AddFunctionVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -15,14 +17,21 @@ return RectorConfig::configure()
     ->withRootFiles()
     ->withPreparedSets(
         codeQuality: true,
+        typeDeclarations: true,
         earlyReturn: true,
     )
     ->withPhpSets()
     ->withAttributesSets(all: true)
     ->withSkip([
+        // Temporarily disable while we're doing typehinting to avoid mixing these in
+        ClassPropertyAssignToConstructorPromotionRector::class,
         ClosureToArrowFunctionRector::class => [
             // Needs to be a traditional function in order to restrict the scope
             __DIR__.'/src/Renderer/HTMLRenderer.php',
+        ],
+        AddFunctionVoidReturnTypeWhereNoReturnRector::class => [
+            // This stub function shouldn't return `void` as it will actually "return" a string
+            __DIR__.'/src/raw_fn_stub.php',
         ],
         ReturnNeverTypeRector::class => [
             // This stub function shouldn't return `never` as it will actually "return" a string

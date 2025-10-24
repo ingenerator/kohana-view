5ad754a42eef0e6eb90dbf0aecb4b441320ad70c
acoulton <andrew@ingenerator.com>
2025-10-24 17:13:03 +0100

    chore: Remove our risky rector rules again now they've done their job

--- rector.php
+++ rector.php
@@ -2,9 +2,6 @@
 
 declare(strict_types=1);
 
-use Ingenerator\RiskyRectorRules\PhpDocToStrictTypes\AddParamTypeFromPhpDocRector;
-use Ingenerator\RiskyRectorRules\PhpDocToStrictTypes\AddPropertyTypeFromPhpDocRector;
-use Ingenerator\RiskyRectorRules\PhpDocToStrictTypes\AddReturnTypeFromPhpDocRector;
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
@@ -43,9 +40,4 @@ return RectorConfig::configure()
     ])
     ->withImportNames(
         removeUnusedImports: true,
-    )
-    ->withRules([
-        AddPropertyTypeFromPhpDocRector::class,
-        AddParamTypeFromPhpDocRector::class,
-        AddReturnTypeFromPhpDocRector::class,
-    ]);
+    );

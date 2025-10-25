540a63442fc185a45c88b7a00ad109021ae405e3
acoulton <andrew@ingenerator.com>
2025-10-25 02:44:27 +0100

    feat: Migrate computed vars that were used faking asymmetric visibility
    We can now just make these public protected(set) and use php's native
    handling for this rather than having to have getter methods.

--- src/ViewModel/PageLayout/AbstractIntermediateLayoutView.php
+++ src/ViewModel/PageLayout/AbstractIntermediateLayoutView.php
@@ -6,23 +6,15 @@ namespace Ingenerator\KohanaView\ViewModel\PageLayout;
 
 use Ingenerator\KohanaView\ViewModel\NestedParentView;
 
-/**
- * @property-read string $child_html
- */
 abstract class AbstractIntermediateLayoutView extends AbstractNestedChildView implements NestedParentView
 {
     /**
      * @var string set at rendering time by the PageLayoutRenderer
      */
-    protected string $child_html;
+    public protected(set) string $child_html;
 
     public function setBodyHtml(string $html): void
     {
         $this->child_html = $html;
-    }
-
-    protected function var_child_html()
-    {
-        return $this->child_html;
     }
 }

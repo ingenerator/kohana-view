b680a8ac0f070eb76b81f8a804e080b940270627
acoulton <andrew@ingenerator.com>
2025-10-25 23:11:37 +0100

    refactor: Extract logic for building view properties
    This is shared between generating computed & display props.

--- v5-migration-tool/tests/e2e/examples/src/DisplayVarsToNativeProperties/SimpleViewWithVariables.expected.php
+++ v5-migration-tool/tests/e2e/examples/src/DisplayVarsToNativeProperties/SimpleViewWithVariables.expected.php
@@ -4,15 +4,11 @@ namespace My\Application\DisplayVarsToNativeProperties;
 
 use Ingenerator\KohanaView\ViewModel\AbstractViewModel;
 
-/**
- * @property-read string $foo
- * @property-read int $other A custom value
- */
 class SimpleViewWithVariables extends AbstractViewModel
 {
     public protected(set) string $foo;
     /**
-     * A custom value
+     * some property we have info about
      */
     public protected(set) int $other;
     public protected(set) mixed $unknown;

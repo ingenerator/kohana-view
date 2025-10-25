40f8ae2a110bf3a477b5b21f247828e5ff9f6d4f
acoulton <andrew@ingenerator.com>
2025-10-25 01:34:18 +0100

    feat: Dectect and migrate cached computed properties

---
+++ v5-migration-tool/tests/e2e/examples/src/DynamicVarsToPropertyHooks/ViewWithCachedVars.expected.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace My\Application\DynamicVarsToPropertyHooks;
+
+use Ingenerator\KohanaView\ViewModel\AbstractViewModel;
+use stdClass;
+
+class ViewWithCachedVars extends AbstractViewModel
+{
+
+    public object $complex_var {
+        get => $this->getCached(__PROPERTY__, $this->var_complex_var(...));
+    }
+    public array $looping_var {
+        get => $this->getCached(__PROPERTY__, $this->var_looping_var(...));
+    }
+    protected function var_complex_var()
+    {
+        $__cached_result__ = new stdClass();
+        return $__cached_result__;
+    }
+
+    protected function var_looping_var()
+    {
+        foreach ([1,2,3] as $i) {
+            $__cached_result__[] = 'a new row -'.$i;
+        }
+
+        return $__cached_result__;
+    }
+
+}

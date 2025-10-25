33969bc1c9fefc3bd5f3e94cdadcbc219933888d
acoulton <andrew@ingenerator.com>
2025-10-25 01:34:18 +0100

    feat: Automatically detect & migrate cached computed view properties

--- v5-migration-tool/src/Rector/StrictTypeFromPropertyTagFactory.php
+++ v5-migration-tool/src/Rector/StrictTypeFromPropertyTagFactory.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Ingenerator\KohanaViewV5MigrationTool\Rector;
 
+use PhpParser\Node;
 use PhpParser\Node\ComplexType;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Name;
-use PhpParser\Node;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PropertyTagValueNode;
 use Rector\PHPStanStaticTypeMapper\Enum\TypeKind;
 use Rector\StaticTypeMapper\StaticTypeMapper;

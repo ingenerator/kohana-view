7edf7bb56a2d4f8cfe2a7542768c5119a4e4f13a
Andrew Coulton <andrew@ingenerator.com>
2025-10-31 23:43:59 +0000

    fix: Implement __toString on UnescapedHtmlSafeString.php
    Avoids IDE and static analysis warnings when using syntax like
    `<?=raw('some string');?>` in a template, by showing that the object
    returned from raw will be stringable.

--- src/OutputValue/UnescapedHtmlSafeString.php
+++ src/OutputValue/UnescapedHtmlSafeString.php
@@ -4,16 +4,23 @@ declare(strict_types=1);
 
 namespace Ingenerator\KohanaView\OutputValue;
 
+use Stringable;
+
 /**
  * The string wrapped by this class will be rendered unescaped into the HTML.
  */
-final readonly class UnescapedHtmlSafeString implements UnescapedSafeHtmlContent
+final readonly class UnescapedHtmlSafeString implements UnescapedSafeHtmlContent, Stringable
 {
     public function __construct(public string $content)
     {
     }
 
     public function renderSafeHtml(): string
+    {
+        return $this->content;
+    }
+
+    public function __toString(): string
     {
         return $this->content;
     }
